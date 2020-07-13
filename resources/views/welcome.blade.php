e3a406d4c70d92c8e6ede271c14b57a6a0fab663
MSI\Fred <abc@abc.com>
2020-07-13 22:46:55 +0800

    css include

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,57 +10,13 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
+          #testid {
+            border: 10px solid #00FFFF;
+            font-size: 50px;
+            font-color: #FFFFFF;
+          }
         </style>
     </head>
     <body>
@@ -83,7 +39,9 @@
                 <div class="title m-b-md">
                     Say Hello World!
                 </div>
-
+                <div id="testid">
+                    測試
+                </div>
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
