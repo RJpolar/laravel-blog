b433242cf7b51a61729ef386fe17e7dc87076f26
MSI\Fred <abc@abc.com>
2020-07-20 18:38:15 +0800

    link db gdb rewrite index add work list

---
+++ app/Works.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Works extends Model
+{
+    //
+    protected $table = 'works';
+    protected $fillable = ['name'];
+    protected $guarded = ['id'];
+
+}
--- database/migrations/2020_07_20_080650_add_name_to_words_table.php
+++ database/migrations/2020_07_20_080650_add_name_to_words_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class AddNameToWordsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::table('works', function (Blueprint $table) {
+            //
+            $table->string('name', 180);
         });
     }
 
@@ -27,6 +26,8 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::table('works', function (Blueprint $table) {
+            //
+        });
     }
 }
